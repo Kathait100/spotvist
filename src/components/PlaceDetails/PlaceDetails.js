0d2ef15e61a5c89f4bcdce2f0c37d3b8d48f45ee
Kathait100 <82964553+Kathait100@users.noreply.github.com>
2022-07-08 00:56:37 +0530

    Update PlaceDetails.js

--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -57,7 +57,7 @@ const PlaceDetails = ({ place, selected, refProp }) => {
       </CardContent>
       <CardActions>
         <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
-         Spot Visit
+          SpotVisit
         </Button>
         <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
           Website
